915a8ab884475d723125a37b62c7d3818aa6afae
alex p <rucup5@yahoo.com>
2020-04-16 12:31:06 +0000

    Add module manifest and config file

---
+++ manifests/init.pp
@@ -0,0 +1,13 @@
+# Manage NTP
+class pbg_ntp {
+ensure_packages(['ntp'])
+file { '/etc/ntp.conf':
+source => 'puppet:///modules/alex_ntp/ntp.conf',
+notify => Service['ntp'],
+require => Package['ntp'],
+}
+service { 'ntp':
+ensure => running,
+enable => true,
+}
+}
